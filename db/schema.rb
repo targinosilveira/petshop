859fb9b51f10168f759187008dafb59e8d4500e4
targino <targino@targino-laptop.(none)>
2010-01-08 01:28:23 -0300

    iniciando o desenvolvimento do layout da aplicação...

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100108035056) do
+ActiveRecord::Schema.define(:version => 20100108041245) do
+
+  create_table "animais", :force => true do |t|
+    t.string   "nome"
+    t.integer  "cliente_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "clientes", :force => true do |t|
     t.string   "nome"
