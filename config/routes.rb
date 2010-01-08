1949151ee191c287bfaa7840bd93da882956ead7
targino <targino@targino-laptop.(none)>
2010-01-08 00:51:31 -0300

    commit inicial do cadastro de clientes...

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :clientes
+
   map.resources :produtos
 
   # The priority is based upon order of creation: first created -> highest priority.
