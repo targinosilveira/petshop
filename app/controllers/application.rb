e600f2ac3ff17a9750cc3a843e08d49990159d81
targino <targino@targino-laptop.(none)>
2010-01-09 00:11:06 -0300

    criação login do sistema.

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -2,14 +2,16 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+
   helper :all # include all helpers, all the time
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => 'c2f7818a32ba49581ba8bbe2e735d5e7'
-  
-  # See ActionController::Base for details 
+
+  # See ActionController::Base for details
   # Uncomment this to filter the contents of submitted sensitive data parameters
-  # from your application log (in this case, all fields with names like "password"). 
+  # from your application log (in this case, all fields with names like "password").
   # filter_parameter_logging :password
 end
+
