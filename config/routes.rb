75fcd1b4aefcfb1f0201a2c8532351b42b7e008b
targino <targino@targino-laptop.(none)>
2010-02-05 02:12:02 -0300

    adicionando o sistema de gerenciamento de logins e usuários.........

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 ActionController::Routing::Routes.draw do |map|
+  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
+  map.login '/login', :controller => 'sessions', :action => 'new'
+  map.register '/register', :controller => 'users', :action => 'create'
+  map.signup '/signup', :controller => 'users', :action => 'new'
+  map.resources :users
+
+  map.resource :session
+
   # The priority is based upon order of creation: first created -> highest priority.
-  map.root :controller => "home"
+  map.root :controller => "sessions", :action => 'new'
   # Sample of regular route:
   #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
   # Keep in mind you can assign values other than :controller and :action
